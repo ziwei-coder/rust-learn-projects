48a41c3e5e0ad12edb696ef939253135ced6a88f
panmin-coder <delipan.mild@gmail.com>
2023-08-12 10:27:13 +0800

    Use sqlx

--- learn-rocket/product-demo/src/main.rs
+++ learn-rocket/product-demo/src/main.rs
@@ -1,21 +1,39 @@
-#[macro_use] extern crate rocket;
+use rocket::serde::json::serde_json::json;
+use rocket::serde::json::Value;
+use rocket::serde::Serialize;
+use rocket::{futures, get, launch, routes};
 
-use rocket_db_pools::{Database, Connection};
-use rocket_db_pools::sqlx::{self, Row};
+use rocket_db_pools::{sqlx, Connection, Database};
+
+use futures::stream::TryStreamExt;
 
 #[derive(Database)]
-#[database("sqlite_logs")]
-struct Logs(sqlx::SqlitePool);
+#[database("sqlite_products")]
+struct DB(sqlx::SqlitePool);
 
-#[get("/<id>")]
-async fn read(mut db: Connection<Logs>, id: i64) -> Option<String> {
-   sqlx::query("SELECT content FROM logs WHERE id = ?").bind(id)
-       .fetch_one(&mut *db).await
-       .and_then(|r| Ok(r.try_get(0)?))
-       .ok()
+#[derive(sqlx::FromRow, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Product {
+    id: i32,
+    title: String,
+    description: String,
+}
+
+#[get("/")]
+async fn list(mut db: Connection<DB>) -> Value {
+    let products = sqlx::query_as::<_, Product>("SELECT id, title, description FROM products")
+        .fetch(&mut *db)
+        .map_ok(|record| record)
+        .try_collect::<Vec<_>>()
+        .await;
+
+    match products {
+        Ok(data) => json!(data),
+        Err(e) => json!(e.to_string()),
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-   rocket::build().attach(Logs::init()).mount("/", routes![read])
+    rocket::build().attach(DB::init()).mount("/", routes![list])
 }
